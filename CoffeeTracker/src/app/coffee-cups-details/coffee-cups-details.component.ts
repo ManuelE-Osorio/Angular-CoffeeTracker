a1673593d33135cc1b0e9a9b5127879589259752
ManuelE-Osorio <aux.manuel.osorio@gmail.com>
2024-04-10 19:46:38 -0500

    More angular

--- CoffeeTracker/src/app/coffee-cups-details/coffee-cups-details.component.ts
+++ CoffeeTracker/src/app/coffee-cups-details/coffee-cups-details.component.ts
@@ -1,9 +1,9 @@
 import { Component, Input, OnInit, input } from '@angular/core';
 import { CoffeeCupsService } from '../coffee-cups.service';
-import { CoffeeCups, CoffeeCupsForm } from '../coffee-cups';
+import { CoffeeCups, CoffeeCupsForm, CoffeeMeasureUnits } from '../coffee-cups';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { Location, NgIf } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Location, NgIf, formatDate } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-coffee-cups-details',
@@ -13,8 +13,15 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './coffee-cups-details.component.css'
 })
 export class CoffeeCupsDetailsComponent implements OnInit{
-  form = new FormGroup<CoffeeCupsForm>();
-  quantity = new FormControl<number>(this.CoffeeCups?.quantity ?? 0);
+  form : FormGroup<CoffeeCupsForm> = new FormGroup<CoffeeCupsForm>({
+    id: new FormControl<number>(0, {nonNullable: true}),
+    quantity: new FormControl<number>(0, { nonNullable: true }),
+    measure: new FormControl<number>(0, { nonNullable: true }),
+    description: new FormControl<string>('', { nonNullable: true }),
+    units: new FormControl<CoffeeMeasureUnits>(CoffeeMeasureUnits.ml, {nonNullable: true}),
+    date: new FormControl<Date>(new Date(), {nonNullable: true})
+  });;
+
 
 
   CoffeeCups? : CoffeeCups
@@ -37,13 +44,32 @@ export class CoffeeCupsDetailsComponent implements OnInit{
         this.CoffeeCups = {
           id: resp.body.id,
           description: resp.body.description,
-          date : new Date(resp.body.date),
+          date : formatDate(resp.body.date, 'yyyy-MM-dd','en'),
           measure : resp.body.measure,
           quantity : resp.body.quantity,
           units : resp.body.units,
         }
+        this.setForm()
       }
     });
+  }
+
+  setForm(){
+    if (this.CoffeeCups != null){
+      this.form.controls.id.setValue(this.CoffeeCups.id)
+      this.form.controls.quantity.setValue(this.CoffeeCups.quantity)
+      this.form.controls.measure.setValue(this.CoffeeCups.measure)
+      this.form.controls.description.setValue(this.CoffeeCups.description)
+      this.form.controls.units.setValue(this.CoffeeCups.units)
+      this.form.controls.date.setValue(this.CoffeeCups.date)
+    }
+    console.log(this.form.controls.date.value)
+    console.log(this.CoffeeCups?.date)
+  }
+
+  submitForm(){
+    console.log(typeof(this.form.controls.date.value))
+    console.log(this.form.value)
   }
 
   goBack() {
